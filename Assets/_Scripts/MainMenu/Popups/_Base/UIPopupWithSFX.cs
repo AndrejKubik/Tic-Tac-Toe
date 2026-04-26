1c4b5236433fef3527883c06cecee9188d556843
AndrejKubik <andrejkk97@gmail.com>
2026-04-26 17:46:44 +0200

    created UI popup with show SFX class

---
+++ Assets/_Scripts/MainMenu/Popups/_Base/UIPopupWithSFX.cs
@@ -0,0 +1,33 @@
+﻿using Snek.AudioManager;
+using Snek.SingletonManager;
+using UnityEngine;
+
+public abstract class UIPopupWithSFX : UIPopup
+{
+    protected SnekSFXManager _sfxManager;
+
+    [SerializeField] protected AudioClip _showSound;
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+
+        _sfxManager = SnekSingletonManager.GetSingleton<SnekSFXManager>();
+    }
+
+    protected override void Validate()
+    {
+        base.Validate();
+
+        if (!_sfxManager)
+            FailValidation("Cannot find SFX manager singleton.");
+
+        if (!_showSound)
+            FailValidation("Show sound not assigned.");
+    }
+
+    protected virtual void OnEnable()
+    {
+        _sfxManager.PlaySound(_showSound);
+    }
+}
