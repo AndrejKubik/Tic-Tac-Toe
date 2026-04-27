f7db2caa2895ec89533d29401b3c639f6f05c396
AndrejKubik <andrejkk97@gmail.com>
2026-04-27 21:40:49 +0200

    modified settings menu layout for all resolutions

---
+++ Assets/_Scripts/Systems/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using Snek.SingletonManager;
+using SnekEditor.SettingsMenu;
+
+public class SettingsMenu : SnekSettingsMenu
+{
+    private ScreenLayoutManager _screenLayoutManager;
+
+    protected override void Initialize()
+    {
+        _screenLayoutManager = SnekSingletonManager.GetSingleton<ScreenLayoutManager>();
+    }
+
+    protected override void Validate()
+    {
+        base.Validate();
+
+        if (!_screenLayoutManager)
+            FailValidation("Cannot find Screen Layout Manager singleton.");
+    }
+}
